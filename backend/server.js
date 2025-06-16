30ebd786babc460d649c92352b90d0ef2c1d02af
Chessman-0852 <chessman.0854@gmail.com>
2025-06-17 00:49:44 +0530

    Fix __dirname resolution in server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-
+import { fileURLToPath } from "url";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -10,9 +10,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Fix for __dirname in ES modules
-
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
