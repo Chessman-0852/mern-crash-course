bf151825c9f9a2210276ebe479fb1ef0f22bf5db
Chessman-0852 <chessman.0854@gmail.com>
2025-06-17 09:25:58 +0530

    Set up build and deploy for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mern-crash-course/",
+  base: "/",
   server: {
     proxy: {
       "/api": {
